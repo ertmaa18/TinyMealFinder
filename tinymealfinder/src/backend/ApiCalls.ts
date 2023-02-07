12885f1cdb106cbe65866fd63e739b7064a6b54d
Martin JURITSCH <jurmab17@htl-kaindorf.at>
2023-02-07 13:32:34 +0100

    fixed bug when no search results

--- tinymealfinder/src/backend/ApiCalls.ts
+++ tinymealfinder/src/backend/ApiCalls.ts
@@ -1,4 +1,3 @@
-import { List } from "@mui/material";
 import MealDto from "../MealDto";
 import { Meal } from "./interfaces/Interfaces";
 
@@ -6,7 +5,7 @@ export async function searchMealsByName(name: string): Promise<MealDto[]> {
 	const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${encodeURI(name)}`)
 
 	const data = await response.json()
-	if (response.ok) {
+	if (response.ok && data?.meals) {
 		//const meals: Meal[] = data?.meals
 		const meals = data?.meals
 		return await meals;
@@ -19,7 +18,7 @@ export async function searchMealsByIngredient(name: string): Promise<MealDto[]> 
 	const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${encodeURI(name)}`)
 
 	const data = await response.json()
-	if (response.ok) {
+	if (response.ok && data?.meals) {
 		//const meals: Meal[] = data?.meals
 		const meals = data?.meals
 		return await meals;
@@ -32,7 +31,7 @@ export async function getAreasList(): Promise<string[]> {
 	const response = await fetch(`https://www.themealdb.com/api/json/v1/1/list.php?a=list`)
 
 	const data = await response.json()
-	if (response.ok) {
+	if (response.ok && data?.meals) {
 		//const meals: Meal[] = data?.meals
 		const meals: string[] = data?.meals.map((m: any) => m.strArea).filter((a: any) => a !== "Unknown")
 		return await meals;
@@ -45,7 +44,19 @@ export async function searchMealsByArea(name: string): Promise<MealDto[]> {
 	const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${encodeURI(name)}`)
 
 	const data = await response.json()
-	if (response.ok) {
+	if (response.ok && data?.meals) {
+		const meals = data?.meals
+		return await meals;
+	} else {
+		return []
+	}
+}
+
+export async function getRandomMeal(): Promise<MealDto[]> {
+	const response = await fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+
+	const data = await response.json()
+	if (response.ok && data?.meals) {
 		const meals = data?.meals
 		return await meals;
 	} else {
