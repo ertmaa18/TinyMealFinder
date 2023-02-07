7a689a359389e921edb9a54bb212d3f04afa776f
Shiorty <76651964+Shiorty@users.noreply.github.com>
2023-02-07 09:54:21 +0100

    basic test

--- tinymealfinder/cypress/component/SearchByName.cy.ts
+++ tinymealfinder/cypress/component/SearchByName.cy.ts
@@ -1,5 +1,19 @@
-describe('SearchByName.cy.ts', () => {
-  it('playground', () => {
-    // cy.mount()
-  })
-})
+import {searchMealsByName} from "./../../src/backend/ApiCalls"
+import { Meal } from "../../src/backend/interfaces/Interfaces";
+
+describe('Mealsearch by name', () => {
+  
+  it('should be id 52771', async () => {
+    expect({a: await testMeal("Arrabiata")}).to.nested.include({'a[0].id': '52771'});
+  });
+});
+
+async function testMeal(text: string): Promise<Meal[]> {
+  return  [{
+    id: '52771', 
+    category: "",
+    ingredients : [],
+    instructions: "",
+    name: ""
+  }];
+}
