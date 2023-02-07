29d4aa5e57bf5e43b602f2903017951a3c66a9ab
Martin JURITSCH <jurmab17@htl-kaindorf.at>
2023-02-07 09:57:00 +0100

    backend update UwU

--- tinymealfinder/src/backend/ApiCalls.ts
+++ tinymealfinder/src/backend/ApiCalls.ts
@@ -1,7 +1,20 @@
 import { Meal } from "./interfaces/Interfaces";
 
 export async function searchMealsByName(name: string): Promise<Meal[]> {
-	const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
+	const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${encodeURI(name)}`)
+
+	const data = await response.json()
+	if (response.ok) {
+		//const meals: Meal[] = data?.meals
+		const meals = data?.meals
+		return await meals;
+	} else {
+		return []
+	}
+}
+
+export async function searchMealsByIngredient(name: string): Promise<Meal[]> {
+	const response = await fetch(`hhttps://www.themealdb.com/api/json/v1/1/filter.php?i=${encodeURI(name) }`)
 
 	const data = await response.json()
 	if (response.ok) {
