7c5751b720ec965b2f3e98f49242d3c98aa98b09
Max Ertl <max.ertl@interad.at>
2023-02-07 15:05:52 +0100

    added more e2e tests

--- tinymealfinder/cypress/e2e/searchByName.cy.ts
+++ tinymealfinder/cypress/e2e/searchByName.cy.ts
@@ -7,11 +7,29 @@ describe("Meal finder ", () => {
     cy.visit("http://localhost:3000");
   });
 
+  it("should be able to search for Spaghetti", () => {
+    cy.get('input[type="search"]').type("Spaghetti");
+    cy.get("#searchInput").click();
+    cy.get("div").should("have.class", "container");
+  });
+
   it("should display details from Spaghetti", () => {
     cy.get('input[type="search"]').type("Spaghetti");
-    cy.contains("Search").click();
+    cy.get("#searchInput").click();
     cy.get("div").should("have.class", "container");
-    cy.contains("Details").click();
-    cy.get(".ReactModal__Content");
+    cy.contains("See Details")
+      .click()
+      .get(".ReactModal__Content")
+      .should("be.visible");
+  });
+
+  it("should go display recipes after search", () => {
+    cy.get('input[type="search"]').type("Spaghetti");
+    cy.get("#searchInput").click();
+    cy.get("div").should("have.class", "container");
+    cy.get("#searchInput").click();
+    cy.get('input[type="search"]').clear();
+    cy.get("#searchInput").click();
+    cy.contains("Corba").should("be.visible")
   });
 });
