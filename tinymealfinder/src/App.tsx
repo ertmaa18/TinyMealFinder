5f8654c1e4e2fc7a123328a6c587b16f56535ff6
Max Ertl <max.ertl@interad.at>
2023-02-14 09:57:13 +0100

    formatted app.tsx and added random meal button

--- tinymealfinder/src/App.tsx
+++ tinymealfinder/src/App.tsx
@@ -1,53 +1,63 @@
-import React, {useState, useRef, useEffect} from "react"
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import 'jquery/dist/jquery.js';
-import AppNavbar from './AppNavbar';
+import React, { useState, useRef, useEffect } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "jquery/dist/jquery.js";
+import AppNavbar from "./AppNavbar";
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import { render } from '@testing-library/react';
-import MealList from './MealList';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { render } from "@testing-library/react";
+import MealList from "./MealList";
 import { MealDto } from "./backend/interfaces/Interfaces";
-import { searchMealsByName, searchMealsByIngredient, getAreasList } from "./backend/ApiCalls";
+import {
+  searchMealsByName,
+  getRandomMeal,
+} from "./backend/ApiCalls";
 
 function App() {
   //meals: die Elemente des State
   //setMeals: die Funktion um den State zu ändern
-  const [meals, setMeals] = useState<Array<MealDto>>([])
+  const [meals, setMeals] = useState<Array<MealDto>>([]);
 
-  // weil sich das empty-array nie ändert, wird 
+  // weil sich das empty-array nie ändert, wird
   // die Funktion genau einmal aufgerufen.
   // zum laden
   useEffect(() => {
     const fetchMeals = async () => {
-      const data = await searchMealsByName('');
+      const data = await searchMealsByName("");
       setMeals(data);
-    }
+    };
 
     setMeals(new Array<MealDto>());
     fetchMeals();
-  }, [])
+  }, []);
 
-
-  var onSubmitCallback = function(e:any){
+  var onSubmitCallback = function (e: any) {
     e.preventDefault();
     search();
     return false;
-  }
+  };
 
-  var search = async function(){
-    let searchInput = (document.getElementById("searchInput") as HTMLInputElement).value
-    let newMeals:Array<MealDto> = await searchMealsByName(searchInput);
+  var search = async function () {
+    let searchInput = (
+      document.getElementById("searchInput") as HTMLInputElement
+    ).value;
+    let newMeals: Array<MealDto> = await searchMealsByName(searchInput);
     setMeals(newMeals);
-  }
+  };
 
-  return <Container>
+  var random = async function () {
+    let newMeals: Array<MealDto> = await getRandomMeal();
+    setMeals(newMeals);
+  };
+
+  return (
+    <Container>
       <Navbar bg="light" expand="lg">
         <Container fluid>
           <Navbar.Brand href="#">TINY MealFinder</Navbar.Brand>
@@ -55,11 +65,10 @@ function App() {
           <Navbar.Collapse id="navbarScroll">
             <Nav
               className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
+              style={{ maxHeight: "100px" }}
               navbarScroll
             >
-            {
-               /*           
+              {/*           
               <Nav.Link href="#action1">Link</Nav.Link>
               <NavDropdown title="Dropdown" id="navbarScrollingDropdown">
                 <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
@@ -71,32 +80,35 @@ function App() {
                   Something else here
                 </NavDropdown.Item>
               </NavDropdown>
-               */
-            }
+               */}
             </Nav>
-            
-            {
-              /*
+
+            {/*
               fetchMealsByName(document.getElementById('searchInput').value)
-              */
-            }
+              */}
+            <Button variant="outline-info" onClick={random}>
+              Random meal
+            </Button>
             <Form className="d-flex" onSubmit={onSubmitCallback}>
               <Form.Control
-                id='searchInput'
+                id="searchInput"
                 type="search"
                 placeholder="Search"
-                className="me-2"
+                className="me-2 ms-2"
                 aria-label="Search"
               />
-              <Button variant="outline-success" onClick={search}>Search</Button>
+              <Button variant="outline-success" onClick={search}>
+                Search
+              </Button>
             </Form>
           </Navbar.Collapse>
         </Container>
       </Navbar>
       <Container fluid>
-        <MealList meals={meals}/>
+        <MealList meals={meals} />
       </Container>
     </Container>
+  );
 }
 
 export default App;
